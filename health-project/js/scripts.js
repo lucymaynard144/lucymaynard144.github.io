641a606cf0e46c7560d2617edf75c8438d59810e
lucymaynard144 <lucymaynard144@gmail.com>
2017-08-11 13:18:46 -0400

    Health Commit 6

--- health-project/js/scripts.js
+++ health-project/js/scripts.js
@@ -3,7 +3,20 @@ $(document).ready(function() {
       $('.btn').removeClass('stepone');
       $('.btn').addClass('steptwo');
       $('#firsttrimester').effect('slide');
-      $('#firsttrimestertext').fadeIn(2500);
-      $('#initialexplainertext').change('#firstexplainertext');
+      $('#firsttrimestertext').fadeIn(2700);
+      $('#initialexplainertext').hide();
+      $('#firstexplainertext').show();
+      $('#buttonone').hide();
+      $('#buttontwo').show();
+    });
+    $('.steptwo').click(function() {
+      $('.btn').removeClass('steptwo');
+      $('.btn').addClass('stepthree');
+      $('#secondtrimester').effect('slide');
+      $('#secondtrimestertext').fadeIn(2700);
+      $('#firstexplainertext').hide();
+      $('#secondexplainertext').show();
+      $('#buttontwo').hide();
+      $('#buttonthree').show();
     });
   });
